e4ecea9344ce889dd6824a41261396bfa1dca249
Lucas Martin-King <lmartinking@gmail.com>
2024-05-18 01:02:28 +1000

    Fix whitespace

--- source/misc/Format.hx
+++ source/misc/Format.hx
@@ -1,9 +1,9 @@
 package misc;
 
 class Format {
-    // Copied from de.polygonal.core.fmt NumberFormat.groupDigits
-    // This code has been removed from newer versions and the old version of the package
-    // does not work with newer versions of Haxe :-(
+	// Copied from de.polygonal.core.fmt NumberFormat.groupDigits
+	// This code has been removed from newer versions and the old version of the package
+	// does not work with newer versions of Haxe :-(
 	public static function groupDigits(x:Int, thousandsSeparator = "."):String {
 		var n:Float = x;
 		var c = 0;
